675f18e3161f9d625b4074d0558a5256aee96fef
Dalmazio Brisinda <dbrisinda@gmail.com>
2017-01-27 19:03:47 -0700

    Updated folder structure to include untracked files.

---
+++ MyApp/MyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Chuck Houpt on 12/19/15.
+//  Copyright © 2015 Example Corp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
